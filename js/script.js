48eecf60635a4ce0975c741dca8210a16329e9da
kang <kang@qq.com>
2014-10-21 18:22:47 +0800

    使用angularjs完善代码结构

--- js/script.js
+++ js/script.js
@@ -1,55 +1,56 @@
 window.onload = function(){
 
-	var now = new Date();
-	var year = now.getFullYear();
-	var month = now.getMonth();
-	var day = now.getDate();
-	var week = now.getDay();
-	var date = document.getElementById('date');
+	// var now = new Date();
+	// var year = now.getFullYear();
+	// var month = now.getMonth();
+	// var day = now.getDate();
+	// var week = now.getDay();
+	// var date = document.getElementById('date');
 
-	switch (week)
-	{
-	case 0:
-	  var zhou="日";
-	  break;
-	case 1:
-	  var zhou="一";
-	  break;
-	case 2:
-	  var zhou="二";
-	  break;
-	case 3:
-	  var zhou="三";
-	  break;
-	case 4:
-	  var zhou="四";
-	  break;
-	case 5:
-	  var zhou="五";
-	  break;
-	case 6:
-	  var zhou="六";
-	  break;
-	}
+	// switch (week)
+	// {
+	// case 0:
+	//   var zhou="日";
+	//   break;
+	// case 1:
+	//   var zhou="一";
+	//   break;
+	// case 2:
+	//   var zhou="二";
+	//   break;
+	// case 3:
+	//   var zhou="三";
+	//   break;
+	// case 4:
+	//   var zhou="四";
+	//   break;
+	// case 5:
+	//   var zhou="五";
+	//   break;
+	// case 6:
+	//   var zhou="六";
+	//   break;
+	// }
 
-	date.innerHTML =  year + ' 年 ' + (month+1) + ' 月 ' + day + ' 日 ' + '  星期' + zhou;
+	// date.innerHTML =  year + ' 年 ' + (month+1) + ' 月 ' + day + ' 日 ' + '  星期' + zhou;
 
 
-	var navbar = document.getElementById('navbar');
-	var navbarlink = navbar.getElementsByTagName('a');
+	// var navbar = document.getElementById('navbar');
+	// var navbarlink = navbar.getElementsByTagName('a');
 
-	for (var i = 0; i < navbarlink.length; i++) {
-		navbarlink[i].flag = false;
-		navbarlink[i].onmouseover = function() {
-			this.style.background = "#fff"; 
-			this.style.color = "#f16913";
-		}
-		navbarlink[i].onmouseout = function() {
+	// for (var i = 0; i < navbarlink.length; i++) {
+	// 	navbarlink[i].flag = false;
+	// 	navbarlink[i].onmouseover = function() {
+	// 		this.style.background = "#fff"; 
+	// 		this.style.color = "#f16913";
+	// 		alert(123);
+	// 	}
+	// 	navbarlink[i].onmouseout = function() {
 
-			this.style.background = "";
-			this.style.color = "#fff";
-		}
-	};
+	// 		this.style.background = "";
+	// 		this.style.color = "#fff";
+	// 	}
+	// };
 
 	var kuaijinavs = document.getElementById('tab-nav').getElementsByTagName('li');
 	var kuaijicons = document.getElementById('tab-main').getElementsByTagName('div');
